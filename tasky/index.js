754597a6cb0c4c4908401b154aec3c591775b4f8
Jose Vargas <josevargasgalan@gmail.com>
2019-05-02 14:13:00 +0200

    MainWindow and icon created

---
+++ tasky/index.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const path = require ('path');
+const { app, BrowserWindow, Tray } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false
+    });
+    mainWindow.loadFile('./src/index.html');
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+    new Tray(iconPath);
+})
